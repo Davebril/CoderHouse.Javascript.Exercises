ef1e6304709a409f5be4685d0412b2db47390aa3
Davebril <68717358+Davebril@users.noreply.github.com>
2020-10-22 01:20:44 -0300

    Add files via upload

---
+++ EjClase02.js
@@ -0,0 +1,23 @@
+// 1. Pedir número mediante prompt() y si es mayor a 1000 mostrar un alert()
+
+var numero = prompt("Ingrese su numero:")
+
+if (numero > 1000) {
+	alert('Su numero ingresado es mayor a 1000')
+}
+
+// 2. Pedir un texto mediante prompt(), y si es igual a "Hola" mostrar un mensaje por console()
+
+var texto = prompt("Ingrese su texto:")
+
+if (texto = "hola") {
+	console.log('Usted dijo hola')
+}
+
+// 3. Pedir un número por prompt() y evaluar si está entre 10 y 50: En caso positivo mostrar un alert()
+
+var number = prompt("Ingrese su numero:")
+
+if (number > 10 && number < 50) {
+	alert('Numero identificado')
+}
